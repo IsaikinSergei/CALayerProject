73939317d65943d09c10cb441f72e56f18aca181
IsaikinSergei <akinfest007@gmail.com>
2021-04-15 00:05:37 +0400

    Added new properties CALayer class on SecondVC

--- CALayerProject/SecondViewController.swift
+++ CALayerProject/SecondViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.layer.cornerRadius = imageView.frame.height / 2
+            imageView.layer.masksToBounds = true
+            let borderColor = UIColor.yellow
+            imageView.layer.borderColor = borderColor.cgColor
+            imageView.layer.borderWidth = 10
+        }
+    }
+    
+    @IBOutlet weak var button: UIButton! {
+        didSet {
+            button.layer.shadowOffset = CGSize(width: 0, height: 5)
+            button.layer.shadowOpacity = 1
+            button.layer.shadowRadius = 5
+            let shadowColor = UIColor.black
+            button.layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
