6834204f24a16fccacfb282ac4e6936f655d9119
IsaikinSergei <akinfest007@gmail.com>
2021-04-16 01:06:13 +0400

    Added gradient on SecondVC

--- CALayerProject/SecondViewController.swift
+++ CALayerProject/SecondViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    var gradientLayer: CAGradientLayer! {
+        didSet {
+            gradientLayer.startPoint = CGPoint(x: 1, y: 0)
+            gradientLayer.endPoint = CGPoint(x: 0, y: 1)
+            let startColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1).cgColor
+            let endColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1).cgColor
+            gradientLayer.colors = [startColor, endColor]
+            gradientLayer.locations = [0.2, 0.8]
+        }
+    }
+    
     @IBOutlet weak var imageView: UIImageView! {
         didSet {
             imageView.layer.cornerRadius = imageView.frame.height / 2
@@ -29,8 +40,16 @@ class SecondViewController: UIViewController {
         }
     }
     
+    override func viewDidLayoutSubviews() {
+        gradientLayer.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        gradientLayer = CAGradientLayer()
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
     }
     
     override func didReceiveMemoryWarning() {
