5a2d547e1ff7b45ca674c1a97201ecc7f04ead3c
IsaikinSergei <akinfest007@gmail.com>
2021-04-20 12:56:26 +0400

    Added CABasicAnimation on the SecondVC

--- CALayerProject/SecondViewController.swift
+++ CALayerProject/SecondViewController.swift
@@ -9,6 +9,28 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    var shapeLayer: CAShapeLayer! {
+        didSet {
+            shapeLayer.lineWidth = 10
+            shapeLayer.lineCap = .round
+            shapeLayer.fillColor = nil
+            shapeLayer.strokeEnd = 1
+            let color = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+            shapeLayer.strokeColor = color
+        }
+    }
+    
+    var overShapeLayer: CAShapeLayer! {
+        didSet {
+            overShapeLayer.lineWidth = 10
+            overShapeLayer.lineCap = .round
+            overShapeLayer.fillColor = nil
+            overShapeLayer.strokeEnd = 0
+            let color = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).cgColor
+            overShapeLayer.strokeColor = color
+        }
+    }
+    
     var gradientLayer: CAGradientLayer! {
         didSet {
             gradientLayer.startPoint = CGPoint(x: 1, y: 0)
@@ -24,9 +46,9 @@ class SecondViewController: UIViewController {
         didSet {
             imageView.layer.cornerRadius = imageView.frame.height / 2
             imageView.layer.masksToBounds = true
-            let borderColor = UIColor.yellow
-            imageView.layer.borderColor = borderColor.cgColor
-            imageView.layer.borderWidth = 10
+//            let borderColor = UIColor.yellow
+//            imageView.layer.borderColor = borderColor.cgColor
+//            imageView.layer.borderWidth = 10
         }
     }
     
@@ -43,6 +65,9 @@ class SecondViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         gradientLayer.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
         
+        configShapeLayer(shapeLayer)
+        configShapeLayer(overShapeLayer)
+        
     }
     
     override func viewDidLoad() {
@@ -50,10 +75,41 @@ class SecondViewController: UIViewController {
         gradientLayer = CAGradientLayer()
         view.layer.insertSublayer(gradientLayer, at: 0)
         
+        shapeLayer = CAShapeLayer()
+        view.layer.addSublayer(shapeLayer)
+        
+        overShapeLayer = CAShapeLayer()
+        view.layer.addSublayer(overShapeLayer)
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
+    
+    func configShapeLayer(_ shapeLayer: CAShapeLayer) {
+        shapeLayer.frame = view.bounds
+        let path = UIBezierPath()
+        path.addArc(withCenter: CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2), radius: imageView.frame.height / 2, startAngle: self.view.frame.width / 2 - 100, endAngle: self.view.frame.width / 2, clockwise: true)
+//        path.move(to: CGPoint(x: self.view.frame.width / 2 - 100, y: self.view.frame.height / 2))
+//        path.addLine(to: CGPoint(x: self.view.frame.width / 2 + 100, y: self.view.frame.height / 2))
+        shapeLayer.path = path.cgPath
+    }
+    
+    @IBAction func tapped(_ sender: UIButton) {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.duration = 20
+        
+//        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+//        animation.fillMode = CAMediaTimingFillMode.both
+        animation.isRemovedOnCompletion = false
+        
+//        animation.delegate = self
+        
+        overShapeLayer.add(animation, forKey: nil)
+    }
+    
 }
